25c6bab302d1ea8d3fc0aba63ad46c7b8057a8a8
mrnlso <46461945+marianoaloiso@users.noreply.github.com>
2024-12-07 20:32:21 -0500

    Refactor MultilingualFinetuningExperiment to integrate ModelExperimentMixin

--- src/transfer_learning/multilingual_finetuning.py
+++ src/transfer_learning/multilingual_finetuning.py
@@ -1,95 +1,20 @@
 from src.data.data_loader import DataLoader
-from src.project_setup import ProjectSetup
 from src.utils.base_experiment import BaseExperiment
-from src.utils.model import calculate_class_weights, load_automodel, WeightedTrainer
-from transformers import TrainingArguments
+from src.utils.model import ModelExperimentMixin
 import logging
 
 logger = logging.getLogger(__name__)
 
-class MultilingualFinetuningExperiment(BaseExperiment):
+class MultilingualFinetuningExperiment(BaseExperiment, ModelExperimentMixin):
     """Fine-tuning model on multilingual dataset"""
 
     def __init__(self, config_path):
         super().__init__(config_path, experiment_type="multilingual_finetuning")
-        self.model_name = self.config.model_name
-        self.num_labels = self.config.num_labels
-        self.model = None
-        self.tokenizer = None
-
-    def train(self, use_class_weights=False):
-        """Train the model on the training dataset
-
-        Args:
-            use_class_weights (bool): Whether to use class weights for training
-        """
-        logger.info("Starting multilingual training")
-
-        class_weights = None
-        if use_class_weights:
-            class_weights = calculate_class_weights(self.datasets["train"].labels)
-
-        training_args = TrainingArguments(
-            output_dir=self.models_dir,
-            num_train_epochs=self.config.num_epochs,
-            per_device_train_batch_size=self.config.batch_size,
-            learning_rate=self.config.learning_rate,
-        )
-
-        trainer = WeightedTrainer(
-            model=self.model,
-            args=training_args,
-            train_dataset=self.datasets["train"],
-            eval_dataset=self.datasets["validation"],
-            class_weights=class_weights,
-        )
-
-        train_results = trainer.train()
-        trainer.save_model(str(self.models_dir))
-
-        self.save_json(
-            train_results.metrics,
-            self.models_dir / "training_metrics.json",
-        )
-
-        logger.info("Completed multilingual training")
-
-        return trainer
-
-    def evaluate(self, trainer, language, dataset_split="test"):
-        """Evaluate the model on the specified dataset split
-
-        Args:
-            trainer (Trainer): Trainer object
-            language (str): Language to evaluate on
-            dataset_split (str): Dataset split to evaluate on
-        
-        Returns:
-            dict: Evaluation metrics
-        """
-        logger.info(f"Evaluating model on {dataset_split} set")
-        
-        predictions = trainer.predict(self.datasets[dataset_split])
-        
-        pred_labels = predictions.predictions.argmax(-1)
-        true_labels = [x["labels"].item() for x in self.datasets[dataset_split]]
-        
-        self.save_predictions(
-            predictions=pred_labels.tolist(),
-            true_labels=true_labels,
-            save_path=self.predictions_dir / f"{language}_predictions_{dataset_split}.json"
-        )
-        
-        metrics = self.calculate_metrics(
-            true_labels=true_labels,
-            predictions=pred_labels.tolist()
-        )
-        
-        return metrics
+        ModelExperimentMixin.__init__(self, self.config)
 
     def run_experiment(self, languages):
         """Run the multilingual fine-tuning experiment"""
-        self.model, self.tokenizer = load_automodel(self.model_name, self.num_labels)
+        self.model, self.tokenizer = self.load_automodel(self.model_name, self.num_labels)
 
         data_loader = DataLoader(
             tokenizer=self.tokenizer,
@@ -98,7 +23,7 @@ class MultilingualFinetuningExperiment(BaseExperiment):
 
         self.datasets = data_loader.load_combined_language_data()
 
-        trainer = self.train(use_class_weights=True)
+        trainer = self.train("combined")
 
         for language in languages:
             logger.info(f"Evaluating experiment for language: {language}")
